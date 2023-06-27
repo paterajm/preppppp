345fb70f84eca77a4459ffeedbfd618ea0fee814
paterajm <134966807+paterajm@users.noreply.github.com>
2023-06-27 15:43:59 -0400

    initial

---
+++ src/main/java/com/mycompany/preppppppp/Preppppppp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.preppppppp;
+
+/**
+ *
+ * @author warrior
+ */
+public class Preppppppp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
